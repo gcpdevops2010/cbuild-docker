2d3eb12e5189a6af782b7fb24c40b52c45e8196c
gcpdevops2010 <149203598+gcpdevops2010@users.noreply.github.com>
2023-11-02 08:36:53 -0700

    Create run-command.sh

---
+++ run-command.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check if at least two arguments were passed
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 {git|mvn|cxscan|sonarscan} [arguments...]"
+    exit 1
+fi
+
+# Extract the command and shift the arguments to pass the rest to the respective tool
+COMMAND="$1"
+shift
+
+# Case statement to handle different commands
+case "$COMMAND" in
+    git)
+        echo "Running Git command..."
+        git "$@"
+        ;;
+    mvn)
+        echo "Running Maven build..."
+        mvn "$@"
+        ;;
+    cxscan)
+        echo "Running Checkmarx scan..."
+        cxscan "$@"
+        ;;
+    sonarscan)
+        echo "Running SonarQube scan..."
+        sonarscan "$@"
+        ;;
+    *)
+        echo "Invalid command: $COMMAND"
+        echo "Usage: $0 {git|mvn|cxscan|sonarscan} [arguments...]"
+        exit 1
+        ;;
+esac
