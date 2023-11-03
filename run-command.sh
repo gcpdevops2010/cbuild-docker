4b39d74f096d9fd52e64b4860ba56656892c39e3
gcpdevops2010 <149203598+gcpdevops2010@users.noreply.github.com>
2023-11-03 09:24:04 -0700

    Update run-command.sh

--- run-command.sh
+++ run-command.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 # Check if at least two arguments were passed
 if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 {git|mvn|cxscan|sonarscan} [arguments...]"
+    echo "Usage: $0 {git|mvn|cx|sonar-scanner|twistcli|docker} [arguments...]"
     exit 1
 fi
 
 # Extract the command and shift the arguments to pass the rest to the respective tool
 COMMAND="$1"
 shift
- 
+
 # Case statement to handle different commands
 case "$COMMAND" in
     git)
@@ -20,17 +20,29 @@ case "$COMMAND" in
         echo "Running Maven build..."
         mvn "$@"
         ;;
-    cxscan)
+    cx)
         echo "Running Checkmarx scan..."
-        cxscan "$@"
+        cx "$@"
         ;;
-    sonarscan)
+
+    sonar-scanner)
         echo "Running SonarQube scan..."
-        sonarscan "$@"
+        sonar-scanner "$@"
         ;;
+
+	twistcli)
+		echo "Running twistcli scan..."
+        twistcli "$@"
+        ;;
+
+	docker)
+		echo "Running docker scan..."
+        docker "$@"
+        ;;	
+		
     *)
         echo "Invalid command: $COMMAND"
-        echo "Usage: $0 {git|mvn|cxscan|sonarscan} [arguments...]"
+        echo "Usage: $0 {git|mvn|cx|sonarscan|twistcli|docker} [arguments...]"
         exit 1
         ;;
 esac
